1e7104e5021b365c045a020be765573c2fc6f8ba
Fiona-T <fitracey@gmail.com>
2021-07-26 16:02:50 +0000

    feat: add get_country function and basic functionality to get country name, run.py

--- run.py
+++ run.py
@@ -42,8 +42,21 @@ def welcome_msg():
         \nHere you can look up the Happiness Score \
         \nper country, for the years 2005 - 2020.\
             \nNote: For some countries data doesn't exist\
-            \nfor every year in the range.") 
+            \nfor every year in the range.")
     print_smiley()
 
 
+def get_country():
+    """
+    Get country name from user
+    """
+    print("\nChoose the country you want to look up.")
+    print("You will be shown the data for that country. \
+        \nThen you can choose to view a graph of that data.")
+    country = input(
+        "Please enter the name of the country you want to look up:\n")
+    print(country)
+
+
 welcome_msg()
+get_country()

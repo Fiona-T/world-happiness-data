bfe620198c63b37d53118f05e85232f435a7949e
Fiona-T <fitracey@gmail.com>
2021-07-26 15:07:18 +0000

    feat: add welcome_msg function, adjust spacing in smiley face to align, run.py

--- run.py
+++ run.py
@@ -5,27 +5,45 @@ from termcolor import colored
 
 
 def print_banner_msg(text):
+    """
+    Print the text passed in, as a banner msg using Figlet font
+    """
     font = Figlet(font="ogre")
-    print("-" * 40)
+    print("-" * 44)
     print(colored(font.renderText(text), "yellow"))
-    print("-" * 40)
+    print("-" * 44)
 
 
 def print_smiley():
-    print("-" * 40)
-    print(" " * 10 + "_" * 8)
-    print(" " * 7 + "_'" + " " * 10 + "'_")
-    print(" " * 5 + "." + " " * 16 + ".")
-    print(" " * 4 + "/" + " " * 5 + "." + " " * 7 + "." + " " * 5 + "\\")
-    print(" " * 3 + ":" + " " * 21 + ":")
-    print(" " * 3 + "|" + " " * 21 + "|")
-    print(" " * 3 + ":" + " " * 3 + "\\" + " " * 13 + "/" + " " * 3 + ":")
-    print(" " * 4 + "\\" + " " * 3 + "`. _______ .'" + " " * 3 + "/")
-    print(" " * 5 + "`." + " " * 15 + ".'")
-    print(" " * 7 + "`_" + " " * 11 + "_'")
-    print(" " * 10 + "' " + "-" * 6 + " '")
-    print("-" * 40)
+    """
+    Print smiley face using ASCII characters
+    """
+    print("-" * 44)
+    print(" " * 18 + "_" * 8)
+    print(" " * 15 + "_'" + " " * 10 + "'_")
+    print(" " * 13 + "." + " " * 16 + ".")
+    print(" " * 12 + "/" + " " * 5 + "." + " " * 7 + "." + " " * 5 + "\\")
+    print(" " * 11 + ":" + " " * 21 + ":")
+    print(" " * 11 + "|" + " " * 21 + "|")
+    print(" " * 11 + ":" + " " * 3 + "\\" + " " * 13 + "/" + " " * 3 + ":")
+    print(" " * 12 + "\\" + " " * 3 + "`. _______ .'" + " " * 3 + "/")
+    print(" " * 13 + "`." + " " * 15 + ".'")
+    print(" " * 15 + "`_" + " " * 11 + "_'")
+    print(" " * 18 + "' " + "-" * 6 + " '")
+    print("-" * 44)
 
 
-print_banner_msg("Welcome")
-print_smiley()
+def welcome_msg():
+    """
+    Print banner msg, welcome text and smiley face
+    """
+    print_banner_msg("Welcome")
+    print("Welcome to the World Happiness Data Tool\
+        \nHere you can look up the Happiness Score \
+        \nper country, for the years 2005 - 2020.\
+            \nNote: For some countries data doesn't exist\
+            \nfor every year in the range.") 
+    print_smiley()
+
+
+welcome_msg()
